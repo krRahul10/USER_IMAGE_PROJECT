6e1ab68497e63c21ba6c1bb6c95c43ed6e99f26a
Rahul <rahul10kr7@gmail.com>
2022-10-01 16:41:36 +0530

    middleware done

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,21 @@
 require("dotenv").config()
+require("./db/conn")
 const express = require("express")
 const app = express()
+const cors = require("cors")
 const router = require("./routes/router")
-require("./db/conn")
+
+
+
 const port = 8080
+
+app.use(express.json())
 app.use(router)
+app.use(cors())
+
+//static path
+
+app.use("/uploads", express.static("./uploads"))
 
 
 app.listen(port,()=>{
--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,10 +1,58 @@
-const express = require("express")
+const express = require("express");
+const multer = require("multer");
+const user = require("../model/userSchema");
+const router = new express.Router();
+const moment = require("moment");
+// router.get("/", (req, res) => {
+//   res.json("server start hai bhai router par");
+// });
 
-const router = new express.Router()
+const imgconfig = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, "./uploads");
+  },
+  filename: (req, file, callback) => {
+    callback(null, `image - ${Date.now()}. ${file.originalname}`);
+  },
+});
 
-router.get("/", (req,res) =>{
-res.json("server start hai bhai router par")
-})
+const isImage = (req, file, callback) => {
+  if (file.mimetype.startswith("image")) {
+    callback(null, true);
+  } else {
+    callback(new Error("Only Images are allowed"));
+  }
+};
 
+const upload = multer({
+  storage: imgconfig,
+  fileFilter: isImage,
+});
 
-module.exports = router
+router.post("/register", upload.single("photo"), async (req, res) => {
+  const { filename } = req.file;
+  const { fname } = req.body;
+
+  if (!fname || !filename) {
+    res.status(401).json({ status: 401, message: "Fill all the details" });
+  }
+  try {
+    const date = moment(new Date()).format("YYYY-MM-DD");
+    const userData = new user({
+      fname: fname,
+      imgPath: filename,
+      date: date,
+    });
+
+    const finalData = await userData.save();
+
+    res.status(201).json({ status: 201, finalData });
+
+  } catch (err) {
+
+    res.status(401).json({ status: 401, err });
+    
+  }
+});
+
+module.exports = router;
